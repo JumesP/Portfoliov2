12bf1ed991fbfa4116e41528e0e7b1be26a76286
James <118614403+JumesP@users.noreply.github.com>
2025-03-08 23:11:35 +0000

    reordered headers
    reordered headers from:
    main, about, projects
    to
    projects, main, about
    
    aim was to move main to the middle

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -8,13 +8,13 @@ const Layout = () => {
 			<nav>
 				<ul>
 					<li>
+						<Link to="/projects">Projects</Link>
+					</li>
+					<li>
 						<Link to="/">Home</Link>
 					</li>
 					<li>
 						<Link to="/about">About</Link>
-					</li>
-					<li>
-						<Link to="/projects">Projects</Link>
 					</li>
 				</ul>
 			</nav>
