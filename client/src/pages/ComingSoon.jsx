8ecf1b0866b95a973aa40ca8c8a210ad30c99f1b
James <118614403+JumesP@users.noreply.github.com>
2025-05-03 19:11:12 +0100

    made a coming soon/ under construction page

---
+++ client/src/pages/ComingSoon.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import './styles/ComingSoon.scss';
+
+const ComingSoon = () => {
+	return (
+		<div className="ComingSoon">
+			<h1>Coming Soon!</h1>
+			<p>This page is under construction. Daddy's Cooking.</p>
+		</div>
+	);
+}
+
+export default ComingSoon;
