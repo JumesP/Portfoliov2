afd620136ad7305f85f90ddd2e3d50e5ec7a17f0
James <118614403+JumesP@users.noreply.github.com>
2025-05-03 19:11:28 +0100

    replaced initial code with the ComingSoon Page

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,28 @@ import Layout from "./pages/Layout";
 import Main from "./pages/Main";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
+import ComingSoon from "./pages/ComingSoon";
 
 function App() {
+
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route index element={<Main />} />
-					<>
-						<Route path="/Main" element={<Main />} />
-						<Route path="/About" element={<About />} />
-						<Route path="/Projects" element={<Projects />} />
-					</>
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<ComingSoon />
 	);
+
+	// return (
+	// 	<BrowserRouter>
+	// 		<Routes>
+	// 			<Route path="/" element={<Layout />}>
+	// 				<Route index element={<Main />} />
+	// 				<>
+	// 					<Route path="/Main" element={<Main />} />
+	// 					<Route path="/About" element={<About />} />
+	// 					<Route path="/Projects" element={<Projects />} />
+	// 				</>
+	// 			</Route>
+	// 		</Routes>
+	// 	</BrowserRouter>
+	// );
 }
 
 export default App;
