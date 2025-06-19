67b59749669e1d94966fcd61416e6c5dd00cc047
James <118614403+JumesP@users.noreply.github.com>
2025-06-19 16:46:24 +0100

    added some pages to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import './App.scss';
+import blob from "./images/blob/blob.svg";
 
 import Layout from "./pages/Layout";
 import Main from "./pages/Main";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
+import Technologies from "./pages/Technologies";
+import Contact from "./pages/Contact";
 import ComingSoon from "./pages/ComingSoon";
+import LinkBox from "./component/molecule/link-box";
 
 function App() {
 
-	return (
-		<ComingSoon />
-	);
-
 	// return (
-	// 	<BrowserRouter>
-	// 		<Routes>
-	// 			<Route path="/" element={<Layout />}>
-	// 				<Route index element={<Main />} />
-	// 				<>
-	// 					<Route path="/Main" element={<Main />} />
-	// 					<Route path="/About" element={<About />} />
-	// 					<Route path="/Projects" element={<Projects />} />
-	// 				</>
-	// 			</Route>
-	// 		</Routes>
-	// 	</BrowserRouter>
+	// 	<ComingSoon />
 	// );
+
+	return (
+		<BrowserRouter>
+			{/* Global blob background */}
+			<div className="global-blob-background">
+				<div className="global-blob">
+					<img src={blob} alt="Decorative blob" />
+				</div>
+				<div className="global-blob">
+					<img src={blob} alt="Decorative blob" />
+				</div>
+			</div>
+
+			<LinkBox />
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Main />} />
+					<>
+						<Route path="/Main" element={<Main />} />
+						<Route path="/About" element={<About />} />
+						<Route path="/Projects" element={<Projects />} />
+						<Route path="/Technologies" element={<Technologies />} />
+						<Route path="/Contact" element={<Contact />} />
+					</>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
