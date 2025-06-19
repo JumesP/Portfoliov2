dacd859099a57224f1d9a79f0817b2aa2c628305
James <118614403+JumesP@users.noreply.github.com>
2025-06-19 16:48:12 +0100

    template contact page

---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contact = () => {
+	return (
+		<div className="contact-page">
+		  <h1>Contact Page</h1>
+		  <p>If you have any questions, feel free to reach out!</p>
+		  <p>
+		    Email: <a href="mailto:example@email.com">example@email.com</a>
+		  </p>
+		</div>
+	)
+}
+
+export default Contact;
